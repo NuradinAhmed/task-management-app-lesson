213f970916d16f79313791992fbbd629099d2ad9
NuradinAhmed <Nuradinahmed2012@gmail.com>
2020-04-12 02:11:55 -0500

    Added Dependency injection and spring context using @annotations

--- src/main/java/com/learnspring/taskmanagementapplesson/persistence/repository/IProjectRepository.java
+++ src/main/java/com/learnspring/taskmanagementapplesson/persistence/repository/IProjectRepository.java
@@ -1,11 +1,13 @@
 package com.learnspring.taskmanagementapplesson.persistence.repository;
 
 import com.learnspring.taskmanagementapplesson.persistence.model.Project;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface IProjectRepository {
-    Optional<Project> findById(Long id);
+public interface IProjectRepository  extends CrudRepository<Project, Long> {
+
+    Optional<Project> findByName(String name);
 
     Project save (Project project);
 }
--- src/test/java/com/learnspring/taskmanagementapplesson/service/impl/ContextIntegrationTest.java
+++ src/test/java/com/learnspring/taskmanagementapplesson/service/impl/ContextIntegrationTest.java
@@ -1,4 +1,26 @@
 package com.learnspring.taskmanagementapplesson.service.impl;
 
-public class ContextIntegrationtest {
+import org.apache.catalina.core.ApplicationContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+public class ContextIntegrationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
+    @Test
+    public void whenContextisLoaded_thenNoExceptions(){
+        //
+    }
+
+
+    @Test
+    public void whenContextisLoaded_thenNoExceptions2(){
+        //
+    }
+
 }
